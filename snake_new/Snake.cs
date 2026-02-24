362bb781d7a8057e16a30741967d500d74c991c9
Filip Hajduch <filip.hajduch03@gmail.com>
2026-02-24 14:07:24 +0100

    Refactor: Přidán enum pro směry hada v Snake.cs

--- snake_new/Snake.cs
+++ snake_new/Snake.cs
@@ -1,40 +1,47 @@
 namespace Snake
 {
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
     // represents the snake - head, body and movement
     class Snake
     {
         public int HeadX { get; set; }
         public int HeadY { get; set; }
         public ConsoleColor HeadColor { get; set; }
-        public string Direction { get; set; }
+        public Direction Direction { get; set; }
 
-        // body segments stored as x and y lists
-        private List<int> _bodyXPositions = new List<int>();
-        private List<int> _bodyYPositions = new List<int>();
+        // body segments stored as coordinates
+        private List<(int x, int y)> _bodySegments = new List<(int x, int y)>();
 
         public Snake(int startX, int startY)
         {
             HeadX = startX;
             HeadY = startY;
             HeadColor = ConsoleColor.Red;
-            Direction = "RIGHT";
+            Direction = Direction.Right;
         }
 
         // number of body segments
-        public int BodyLength => _bodyXPositions.Count;
+        public int BodyLength => _bodySegments.Count;
 
         // get body segment position at index
         public (int x, int y) GetBodySegment(int index)
         {
-            return (_bodyXPositions[index], _bodyYPositions[index]);
+            return _bodySegments[index];
         }
 
         // check if head overlaps any body segment
         public bool IsCollidingWithSelf()
         {
-            for (int i = 0; i < _bodyXPositions.Count; i++)
+            for (int i = 0; i < _bodySegments.Count; i++)
             {
-                if (_bodyXPositions[i] == HeadX && _bodyYPositions[i] == HeadY)
+                if (_bodySegments[i].x == HeadX && _bodySegments[i].y == HeadY)
                     return true;
             }
             return false;
@@ -43,21 +50,20 @@ namespace Snake
         // save current head position to body and move head
         public void Move()
         {
-            _bodyXPositions.Add(HeadX);
-            _bodyYPositions.Add(HeadY);
+            _bodySegments.Add((HeadX, HeadY));
 
             switch (Direction)
             {
-                case "UP":
+                case Direction.Up:
                     HeadY--;
                     break;
-                case "DOWN":
+                case Direction.Down:
                     HeadY++;
                     break;
-                case "LEFT":
+                case Direction.Left:
                     HeadX--;
                     break;
-                case "RIGHT":
+                case Direction.Right:
                     HeadX++;
                     break;
             }
@@ -66,38 +72,36 @@ namespace Snake
         // remove oldest body segment if body is longer than allowed
         public void TrimTail(int maxLength)
         {
-            if (_bodyXPositions.Count > maxLength)
+            if (_bodySegments.Count > maxLength)
             {
-                _bodyXPositions.RemoveAt(0);
-                _bodyYPositions.RemoveAt(0);
+                _bodySegments.RemoveAt(0);
             }
         }
 
         // try to change direction, prevents reversing
         public bool TryChangeDirection(ConsoleKey key)
         {
-            if (key == ConsoleKey.UpArrow && Direction != "DOWN")
+            if (key == ConsoleKey.UpArrow && Direction != Direction.Down)
             {
-                Direction = "UP";
+                Direction = Direction.Up;
                 return true;
             }
-            if (key == ConsoleKey.DownArrow && Direction != "UP")
+            if (key == ConsoleKey.DownArrow && Direction != Direction.Up)
             {
-                Direction = "DOWN";
+                Direction = Direction.Down;
                 return true;
             }
-            if (key == ConsoleKey.LeftArrow && Direction != "RIGHT")
+            if (key == ConsoleKey.LeftArrow && Direction != Direction.Right)
             {
-                Direction = "LEFT";
+                Direction = Direction.Left;
                 return true;
             }
-            if (key == ConsoleKey.RightArrow && Direction != "LEFT")
+            if (key == ConsoleKey.RightArrow && Direction != Direction.Left)
             {
-                Direction = "RIGHT";
+                Direction = Direction.Right;
                 return true;
             }
             return false;
         }
     }
 }
-
